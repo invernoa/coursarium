e6ddb069bf8dc83a9ef58e92c1a806ee00993fcf
inverno <martiniras@mail.ru>
2018-02-16 15:10:02 +0300

    settings

--- coursarium/settings.py
+++ coursarium/settings.py
@@ -58,9 +58,9 @@ WSGI_APPLICATION = 'coursarium.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'catalogdb',
-        'USER': 'djangouser',
-        'PASSWORD': '11111'
+        'NAME': 'coloroeu_cm',
+        'USER': 'coloroeu_cm',
+        'PASSWORD': '111111'
         }
     }
 
