e937ef5fdd28eb5e2ea20a51a08ec94fdfd3e9ed
inverno <martiniras@mail.ru>
2018-02-17 03:33:00 +0300

    course detail

--- catalog/views.py
+++ catalog/views.py
@@ -7,11 +7,27 @@ def index(request):
     courses = Course.objects.all().order_by('id')[1:10]
     categories = Category.objects.all()
     startingsoon = Course.objects.all().filter(status=2)
+    courses_categories1 = Course.objects.all().filter(category=1)
+    courses_categories2 = Course.objects.all().filter(category=2)
+    courses_categories3 = Course.objects.all().filter(category=3)
+    courses_categories4 = Course.objects.all().filter(category=4)
+    courses_categories5 = Course.objects.all().filter(category=5)
+    courses_categories6 = Course.objects.all().filter(category=6)
+    courses_categories7 = Course.objects.all().filter(category=7)
+    courses_categories8 = Course.objects.all().filter(category=8)
+    courses_categories9 = Course.objects.all().filter(category=9)
+    courses_categories10 = Course.objects.all().filter(category=1)
+
     #Render the HTML template index.html with the data in the context variable.
     return render(
         request,
         'index.html',
-        context = {'courses': courses, 'categories': categories, 'startingsoon': startingsoon},
+        context = {'courses': courses, 'categories': categories, 'startingsoon': startingsoon,
+                   'courses_categories1': courses_categories1, 'courses_categories2': courses_categories2,
+                   'courses_categories3': courses_categories3,'courses_categories4': courses_categories4,
+                   'courses_categories5': courses_categories5,'courses_categories6': courses_categories6,
+                   'courses_categories7': courses_categories7,'courses_categories8': courses_categories8,
+                   'courses_categories9': courses_categories9, 'courses_categories10': courses_categories10, },
     )
 
 
